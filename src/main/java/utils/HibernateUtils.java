1b9318df58a8880bf8532f6d1654003ab6d59db9
edionu97 <onu_edy@yahoo.com>
2019-03-23 08:30:36 +0200

    Created project Seted project sdk and bytecode to java 11 Created packages controller, model, persistence, utils Created HibernateUtils class Created Main class

---
+++ src/main/java/utils/HibernateUtils.java
@@ -0,0 +1,45 @@
+package utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+
+    /**
+     * Gets the an instance of hibernate utils class
+     * @return an instance
+     */
+    public static HibernateUtils getInstance(){
+
+        if(persistenceUtils == null){
+            synchronized (HibernateUtils.class){
+                if(persistenceUtils == null){
+                    persistenceUtils = new HibernateUtils();
+                }
+            }
+        }
+        return persistenceUtils;
+    }
+
+    /**
+     * Get an object though it we can make sessions with hibernate
+     * @return an instance of that object
+     */
+
+    public SessionFactory getSessionFactory(){
+        return  sessionFactory;
+    }
+
+    /**
+     * Creates the session factory
+     */
+    private HibernateUtils(){
+        sessionFactory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
+
+    
+    private  SessionFactory sessionFactory;
+    private static volatile HibernateUtils persistenceUtils = null;
+}
