7dad143000ffc3bd56935690595b8eee7f2d89e9
edionu97 <onu_edy@yahoo.com>
2019-05-29 22:22:36 +0300

    Created database sql for geting absent students

--- src/main/java/application/database/implementation/HistoryRepoImpl.java
+++ src/main/java/application/database/implementation/HistoryRepoImpl.java
@@ -3,6 +3,8 @@ package application.database.implementation;
 import application.database.AbstractRepoImpl;
 import application.database.interfaces.IHistoryRepo;
 import application.model.History;
+import application.model.User;
+import application.utils.model.ClassType;
 import org.hibernate.Session;
 
 import java.util.ArrayList;
@@ -21,7 +23,7 @@ public class HistoryRepoImpl extends AbstractRepoImpl<History> implements IHisto
                             "h.grp, " +
                             "h.teacherName, " +
                             "h.attendanceImage) " +
-                    "from History h";
+                            "from History h";
 
             return session.createQuery(HQL).getResultList();
         }
@@ -44,11 +46,11 @@ public class HistoryRepoImpl extends AbstractRepoImpl<History> implements IHisto
                             "c.type, " +
                             "student.username, " +
                             "student.role)" +
-                    "from History h " +
+                            "from History h " +
                             "inner join h.attendances a " +
                             "inner join a.course c " +
                             "inner join a.user student " +
-                    "where h.teacherName=:teacher and h.historyId =:historyId ";
+                            "where h.teacherName=:teacher and h.historyId =:historyId ";
 
             return session
                     .createQuery(HQL)
@@ -68,7 +70,7 @@ public class HistoryRepoImpl extends AbstractRepoImpl<History> implements IHisto
                             "h.grp, " +
                             "h.teacherName, " +
                             "h.attendanceImage) " +
-                    "from History h where h.teacherName=:teacher and h.grp=:class";
+                            "from History h where h.teacherName=:teacher and h.grp=:class";
 
             return session
                     .createQuery(HQL)
@@ -77,6 +79,48 @@ public class HistoryRepoImpl extends AbstractRepoImpl<History> implements IHisto
                     .getResultList()
                     .stream()
                     .findFirst();
+        }
+    }
+
+    @Override
+    public List<User> getAbsentUsers(
+            final String teacherName, final String courseName, final ClassType courseType, final String grupa, final int historyId) {
+
+        try (final Session session = persistenceUtils.getSessionFactory().openSession()) {
+
+            //get enrolled students
+            final String HQL =
+                    "select new application.model.query.UserPart(" +
+                            "student.username, " +
+                            "student.role, " +
+                            "student.userId) from Enrollment e " +
+                            "inner join e.course c " +
+                            "inner join e.user student " +
+                            "inner join c.user teacher " +
+                            "where c.name =:courseName and c.type=:courseType and teacher.username=:teacherName and e.group=:grupa";
+            final List<User> absents = session
+                    .createQuery(HQL)
+                    .setParameter("courseType", courseType)
+                    .setParameter("courseName", courseName)
+                    .setParameter("teacherName", teacherName)
+                    .setParameter("grupa", grupa).getResultList();
+
+            //get present students
+            final String HQL2 =
+                    "select a.user.userId from History h " +
+                            "inner join h.attendances a " +
+                    "where a.history.historyId = :historyId";
+            final List<Integer> presentStudents = session
+                    .createQuery(HQL2)
+                    .setParameter("historyId", historyId)
+                    .getResultList();
+
+            //remove present users
+            presentStudents.forEach(x -> {
+                absents.removeIf(y -> y.getUserId() == x);
+            });
+
+            return absents;
         }
     }
 }
